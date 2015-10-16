7ce58482b408137cf2d24e85cc9b090a380ac3a1
winsonnxl <winsonnxl@126.com>
2015-10-16 16:21:16 +0800

    Rejected commit: Default

--- routes/work_index.js
+++ routes/work_index.js
@@ -4,33 +4,51 @@
  */
 var express = require('express');
 var router = express.Router();
-
+var m_bug=require('../models/m_bug');
 
 
 /*工作平台首页*/
 router.get('/',function(req,res,next){
     try{
-        if(req.session.uid>0||req.session.uid!=undefined){
-            var data=[];
-            /*
-            data[session]={uid:req.session.uid,
-                rname:req.session.rname,
-                local:req.session.local,
-                dev:req.session.dev,
-                level:req.session.level
-            };*/
-            data={uid:req.session.uid,
-                rname:req.session.rname,
-                local:req.session.local,
-                dev:req.session.dev,
-                level:req.session.level};
-            console.log(data);
+        if(req.session.uid==undefined||req.session.uid==''){
+            res.render('user/login',{data:'0'});
+        }else {
+            var session=require('../lib/c_session').get_Session(req);
+            var data={'session':session};
             res.render('index',data);
         }
 
     }catch(ex){
         console.log(ex);
         res.send("登录失败楼！工作首页进不去！")
+    }
+
+});
+
+
+/*我的工作页*/
+router.get('/mywork',function(req,res,next){
+    var data={};
+    try{
+        if(req.session.uid==undefined||req.session.uid==''){
+            res.render('user/login',{data:'0'});
+        }else {
+            var session=require('../lib/c_session').get_Session(req);
+
+            m_bug.showBUG(req.session.uid,req.session.leve,req.session.local,req.session.dev,function(result){
+                if(result){
+                    data={'session':session,
+                    'results':result};
+                    console.log('mywork====>',data);
+                    res.render('work/mywork',data);
+                }
+            });
+
+        }
+
+    }catch(ex){
+        console.log(ex);
+        res.send("登录失败楼！我的工作 进不去！")
     }
 
 });
